337a8a68229c87fb2771e8126499103b57fd419d
DaviFerreira14 <davi073ferreira@gmail.com>
2025-08-06 20:37:54 -0300

    feat: separar backend em projeto independente e atualizar URLs

--- src/app/config/environment.ts
+++ src/app/config/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://portfolio-daviferreira.vercel.app/api'
+  apiUrl: 'https://backend-vercel-3jsdslvg6-daviferreira14s-projects.vercel.app/api'
 };
